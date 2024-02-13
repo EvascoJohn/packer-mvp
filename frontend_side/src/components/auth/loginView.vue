bc51310572e7836b9b9e04ed484b72d92df19a18
ShiinaNeet <72781583+ShiinaNeet@users.noreply.github.com>
2024-02-13 23:26:59 +0800

    Update loginView.vue

--- frontend_side/src/components/auth/loginView.vue
+++ frontend_side/src/components/auth/loginView.vue
@@ -36,7 +36,11 @@
           </div>
   
           <div>
-            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+            <button type="submit" class="flex w-full justify-center items-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              <span>Sign in</span>
+          </button>
+
+
           </div>
         </form>
         
@@ -85,8 +89,8 @@
             class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             v-else
             >
-                Register
-            </button>
+                Register 
+          </button>
           </div>
         </form>
         <p class="mt-10 text-center text-sm text-gray-500">
